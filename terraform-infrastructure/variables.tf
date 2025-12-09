db16d293be8c5018b3813b278e984522d20f8901
Olaniyi <ayodelebwj@gmail.com>
2025-12-09 00:19:17 -0500

    updated deployment  jenkins files

--- terraform-infrastructure/variables.tf
+++ terraform-infrastructure/variables.tf
@@ -1,12 +1,3 @@
-variable 
-# CREATE Java instance
-resource "aws_instance" "java_instance" {
-  ami             = data.aws_ami.java-ami.id
-  instance_type   = "t3.micro"              
-  key_name        = "ohio-key"               
-  security_groups = [aws_security_group.java_sg.name]
-
-  tags = {
-    Name = "java-Instance"
-  }
+variable "enikeni" {
+    type = string
 }

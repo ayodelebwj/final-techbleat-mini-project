df7f855ab51f52185358abf911920d8b3ff2eca0
Olaniyi <ayodelebwj@gmail.com>
2026-01-07 03:44:41 -0500

    pushing to repo

---
+++ jenkins-server/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#THIS COMMANDS CREATES THE JENKINS SERVER
+terraform init
+terraform fmt
+terraform validate
+terraform plan -var-file="values.tfvars"
+terraform apply -var-file="values.tfvars" --auto-approve
